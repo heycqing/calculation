23152bfe2b83bf50a3cf665f1a45f4ac29e85c91
cqing <SiqingIII@163.com>
2018-10-12 22:00:21 +0800

    完成升级版，升级可以进行常规的浮点数计算

--- index.js
+++ index.js
@@ -13,20 +13,57 @@
     }
 })(this,(function(){
     'use strict'
+    /* update
+    * js的浮点数运算会不准确；想要准确地算出浮点数，可以直接把小数变化整数，再运算；
+    * 或者直接进行移位运算，这也是解决js浮点数不准确的方法；
+    */
     var calculate = {
-        
+        numLength: function(num){
+            return (num.toString().split('.')[1] || '').length;
+        },
+        tenPow: function(num_a,num_b){
+            return Math.pow(10,Math.max(num_a,num_b));
+        },
+        num2String:function(num){
+            return num.toString();
+        },
+        numTurn: function(str){
+            try{
+                if(str === '0'){
+                    return Number(0);
+                }else if(str > 0){
+                    var temp = this.num2String(str);
+                    return  Number(temp.replace('.',''));
+                }
+            }catch(e){
+                console.log('error!')
+            }
+        },
         add:function(a , b){
-            return a+b;
+            var aLength = this.numLength(a);
+            var bLength = this.numLength(b);
+            var maxNum = this.tenPow(aLength,bLength);
+            return ( a * maxNum + b * maxNum ) / maxNum;
         },
         sub:function(a , b){
-            return a > b ? a - b :b - a;
+            var aLength = this.numLength(a);
+            var bLength = this.numLength(b);
+            var maxNum = this.tenPow(aLength,bLength);
+            return a > b ? ((a * maxNum - b * maxNum) / maxNum) :((b * maxNum - a * maxNum) / maxNum);
         },
-
         mult:function(a , b){
-            return a * b ;
+            var aLength = this.numLength(a);
+            var bLength = this.numLength(b);
+            var a2num = this.numTurn(a);
+            var b2num = this.numTurn(b);
+            return (a2num * b2num)  / Math.pow(10,(aLength + bLength));
         },
         divi: function(a , b){
-            return a / b ;
+            var aLength = this.numLength(a);
+            var bLength = this.numLength(b);
+            var a2num = this.numTurn(a);
+            var b2num = this.numTurn(b);
+            return (a2num / b2num) / Math.pow(10,(aLength - bLength));
         }
     }
     return calculate;
--- test/index-test.js
+++ test/index-test.js
@@ -2,16 +2,22 @@ var calc = require('../index');
 var expect = require('chai').expect;
 
 describe('测试这个插件的运算',function(){
-    it('加法运算，3 + 4 应该等于 7 ', function(){
-        expect(calc.add(3,4)).to.be.equal(7);
+    it('加法运算，3.0 + 4.1 应该等于 7.1 ', function(){
+        expect(calc.add(3.0,4.1)).to.be.equal(7.1);
     });
-    it('减法运算，4 - 3 应该等于 1 ', function(){
-        expect(calc.sub(3,4)).to.be.equal(1);
+    it('减法运算，4.5 - 3.2 应该等于 1.3', function(){
+        expect(calc.sub(4.5,3.2)).to.be.equal(1.3);
     });
-    it('乘法运算，3 * 4 应该等于 12 ', function(){
-        expect(calc.mult(3,4)).to.be.equal(12);
+    it('乘法运算，3.2 * 4.2 应该等于 13.44 ', function(){
+        expect(calc.mult(3.2,4.2)).to.be.equal(13.44);
     });
-    it('除法运算，3 / 4 应该等于 0.75 ', function(){
-        expect(calc.divi(3,4)).to.be.equal(0.75);
+    it('除法运算，0.81 / 0.9 应该等于 0.9 ', function(){
+        expect(calc.divi(0.81,0.9)).to.be.equal(0.9);
     });
+    it('numTurn函数检验',function(){
+        expect(calc.numTurn(5)).to.be.equal(5);
+    })
+    it('numLength 函数检验',function(){
+        expect(calc.numLength(5)).to.be.equal(0)
+    })
 })
