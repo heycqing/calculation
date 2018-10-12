212bec99bb2bbd0d5a8799342ea8cb1b905160a3
cqing <SiqingIII@163.com>
2018-10-12 16:53:49 +0800

    新增了[AMD CMD CommonJs 原生浏览器]的封装写法

--- calculation-js.js
+++ calculation-js.js
@@ -1,4 +1,17 @@
-(function(){
+(function(global,factory){
+    // commonJS 规范
+    if (typeof module === 'object' && module && module.exports) {
+        module.exports = factory(calculate);
+    }
+    // AMD规范
+    else if (typeof define === 'function' && define.amd) {
+        define([calculate], factory);
+    }
+    // 浏览器实现
+    else {
+        global.calculate= factory(global.calculate);
+    }
+})(this,(function(){
     var calculate = {
         
         add:function(a , b){
@@ -15,8 +28,5 @@
             return a / b ;
         }
     }
-
-   this.calc = calculate;
-    // module.exports = calculate;
-
-})()
+    return calculate;
+}))
